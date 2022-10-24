bd2bac8b538a83a7b7d4ab0e21e8ec38ac29250c
Joseph Sun <jsphsun@gmail.com>
2022-10-24 12:33:42 -0400

    Added the basic constructor for the class.

--- src/main/java/CoreEntities/GameParts/LetterRack.java
+++ src/main/java/CoreEntities/GameParts/LetterRack.java
@@ -4,8 +4,14 @@ import java.io.Serializable;
 
 public class LetterRack implements Serializable {
 
+    private Bag bagReference;
     private Tile[] letters;
     final int RACK_LEN = 7;
+
+    //Default Constructor
+    public LetterRack(){
+
+    }
 
     public boolean refill(){
 
