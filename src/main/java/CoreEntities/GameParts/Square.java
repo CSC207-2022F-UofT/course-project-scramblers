9f9da80269400d7a66679b2a04bfc2df5ed4738e
ethanhonest <ethan.honest@mail.utoronto.ca>
2022-10-31 11:20:06 -0400

    updated getters and setters for square

--- src/main/java/CoreEntities/GameParts/Square.java
+++ src/main/java/CoreEntities/GameParts/Square.java
@@ -1,7 +1,20 @@
 package CoreEntities.GameParts;
 
 public class Square {
-    public Square () {
+    private final String multiplier;
+    private Tile tile;
 
+    public Square (String multiplier, Tile tile) {
+        this.multiplier = multiplier;
+        this.tile = tile;
+    }
+    public void setTile(Tile newTile) {
+        this.tile = newTile;
+    }
+    public Tile getTile() {
+        return this.tile;
+    }
+    public String getMultiplier() {
+        return this.multiplier;
     }
 }
