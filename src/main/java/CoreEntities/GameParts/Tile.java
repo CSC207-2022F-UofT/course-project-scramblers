52af023f93454e90f4ed9dd5db2b9852285ad219
patelhrid <patelhrid@gmail.com>
2022-10-31 11:37:57 -0400

    Created Bag and Tile with required core methods

--- src/main/java/CoreEntities/GameParts/Tile.java
+++ src/main/java/CoreEntities/GameParts/Tile.java
@@ -1,4 +1,19 @@
 package CoreEntities.GameParts;
 
 public class Tile {
+    private final String letter;
+    private final int value;
+
+    public Tile(String letter, int value) {
+        this.letter = letter;
+        this.value = value;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
