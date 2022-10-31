0f787c7cd23ff97411f2a5e3d8e1295e5afed247
BlueFide <djjcumberbatch@gmail.com>
2022-10-31 11:13:22 -0400

    Created core Player Class

--- src/main/java/CoreEntities/Player/HumanPlayer.java
+++ src/main/java/CoreEntities/Player/HumanPlayer.java
@@ -1,4 +1,19 @@
 package CoreEntities.Player;
 
+import CoreEntities.GameParts.LetterRack;
+
 public class HumanPlayer extends Player {
+    public HumanPlayer(String name, LetterRack rack, int score) {
+        super(name, rack, score);
+    }
+
+    @Override
+    boolean makeMove(String word) {
+        return false;
+    }
+
+    @Override
+    void replaceLetters(char[] toReplaceValues, char[] newValues) {
+
+    }
 }
