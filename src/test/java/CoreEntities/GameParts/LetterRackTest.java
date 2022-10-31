7e0834dce183430ca9080ff7a136b27fd972892e
Joseph Sun <jsphsun@gmail.com>
2022-10-31 14:31:36 -0400

    Initial Version of the test, yet to work.

---
+++ src/test/java/CoreEntities/GameParts/LetterRackTest.java
@@ -0,0 +1,36 @@
+package CoreEntities.GameParts;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+class LetterRackTest {
+
+    @Test
+    public void GeneralCase() {
+        //instantiating needed objects
+        Bag bag = new Bag();
+        LetterRack letterRack = new LetterRack(bag);
+        Tile[] tileArray = letterRack.getLETTERS();
+        Random rand = new Random();
+        rand.setSeed(0);
+
+        //Testing removeLetters
+        int removedIndex = rand.nextInt(tileArray.length);
+        //admittedly weird string conversion
+        String toRemove = "" + tileArray[removedIndex].getLetter();
+        letterRack.removeLetters(toRemove);
+        Assertions.assertNull(letterRack.getLETTERS()[removedIndex]);
+
+        //Testing rackIsNotFull
+        Assertions.assertTrue(letterRack.rackNotFull());
+
+        //Testing refill
+        letterRack.refill();
+        for(Tile tile: letterRack.getLETTERS()){
+            Assertions.assertNotNull(tile);
+        }
+
+    }
+}
