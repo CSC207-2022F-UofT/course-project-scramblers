677877a761939f10640e392bdce0849ea7be5a45
patelhrid <patelhrid@gmail.com>
2022-10-31 17:32:50 -0400

    Created basic test functions, body not implemented for each.

---
+++ src/test/java/CoreEntities/GameParts/BagTest.java
@@ -0,0 +1,23 @@
+package CoreEntities.GameParts;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BagTest {
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void pop() {
+    }
+
+    @Test
+    void distribute() {
+    }
+
+    @Test
+    void add() {
+    }
+}
