aa9e44abd62cc982ac11df7fe590c49ae191ccef
Joseph Sun <jsphsun@gmail.com>
2022-10-31 12:00:39 -0400

    Finished rackNotFull() method and Constructor. Working on findTile.

--- src/main/java/CoreEntities/GameParts/LetterRack.java
+++ src/main/java/CoreEntities/GameParts/LetterRack.java
@@ -1,6 +1,7 @@
 package CoreEntities.GameParts;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class LetterRack implements Serializable {
 
@@ -10,18 +11,41 @@ public class LetterRack implements Serializable {
 
     //Default Constructor
     public LetterRack(Bag inputBag) {
-
+        this.bagReference = inputBag;
+        this.letters = new Tile[RACK_LEN];
+        for (int index = 0; index < RACK_LEN; index++){
+            this.letters[index] = this.bagReference.pop();
+        }
     }
 
     public boolean refill(){
 
     }
 
-    public boolean removeLetters(){
+    /**
+     *  Removes letters based on a given string
+     * @param inputWord have all letters in the letter rack
+     * @return nothing but object is modified with the letters specified removes
+     */
+    public boolean removeLetters(String inputWord){
+        for(char characterInWord: inputWord){
+            this.letters[]
+        }
+    }
 
+    private int findTile(char inputChar){
+        int output = 0;
+        for(Tile tile: letters){
+            if(tile)
+        }
     }
 
     public boolean rackNotFull(){
-
+        for(Tile tile: letters){
+            if(tile == null){
+                return true;
+            }
+        }
+        return false;
     }
 }
