a757df957e22162fc62692232dfec761260876ac
JosephUofT <byu.hamdawy>
2022-10-31 11:53:27 -0400

    setUpView method created

--- src/main/java/CoreEntities/IO/View.java
+++ src/main/java/CoreEntities/IO/View.java
@@ -8,4 +8,19 @@ public class View{
     private int height;
     private int width;
 
+    public View(int w, int h){
+
+        frame = new JFrame();
+        width = w;
+        height = h;
+    }
+
+    public void setUpView(){
+        frame.setSize(width, height);
+        frame.setTitle("SCRABBLE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
 }
