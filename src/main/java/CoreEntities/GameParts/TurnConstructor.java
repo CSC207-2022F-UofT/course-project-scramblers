de364c424b42ad82ea225a2c48ba963c9d655c0e
pete johansson <petesaiacct@gmail.com>
2022-11-01 16:36:21 -0400

    Created class turnConstructor with a description of its recipes to call for a turn cycle.

---
+++ src/main/java/CoreEntities/GameParts/TurnConstructor.java
@@ -0,0 +1,8 @@
+package CoreEntities.GameParts;
+//    This class contains a 'recipe' for each turn cycle,
+//    These are validate_board_placement, validate_tiles, validate_word,
+//    update_removed_tiles, calculate_score, update_score
+
+public class TurnConstructor {
+
+}
