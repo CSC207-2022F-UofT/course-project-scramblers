a1ae4d1b2d27a25cedcb6c40d5417c103e9450b0
petejohansson <petesaiacct@gmail.com>
2022-11-01 21:05:39 -0400

    Created Dictionary class. This class will initiate the reading of the dictionary text file, and create a searchable object for game use.

---
+++ src/main/java/CoreEntities/GameParts/Dictionary.java
@@ -0,0 +1,9 @@
+package CoreEntities.GameParts;
+/* This class reads the dictionary.txt file and imports it as a searchable file for use to confirm
+words with verify_word for validity to place on board.
+
+
+ */
+
+public class Dictionary {
+}
