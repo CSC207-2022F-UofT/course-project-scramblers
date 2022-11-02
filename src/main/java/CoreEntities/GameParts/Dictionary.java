d4b3a7db454420a4b3bba10d5139632433006a2f
petejohansson <petesaiacct@gmail.com>
2022-11-01 21:23:39 -0400

    Comment and TODO updates

--- src/main/java/CoreEntities/GameParts/Dictionary.java
+++ src/main/java/CoreEntities/GameParts/Dictionary.java
@@ -6,4 +6,11 @@ words with verify_word for validity to place on board.
  */
 
 public class Dictionary {
+
+    /*
+    TODO Establish data type with co-members, create constructor style to read file and create object.
+
+    TODO Create search methods for length and character sets.
+
+     */
 }
--- src/main/java/CoreEntities/GameParts/TurnConstructor.java
+++ src/main/java/CoreEntities/GameParts/TurnConstructor.java
@@ -1,7 +1,10 @@
 package CoreEntities.GameParts;
-//    This class contains a 'recipe' for each turn cycle,
-//    These are validate_board_placement, validate_tiles, validate_word,
-//    update_removed_tiles, calculate_score, update_score
+
+/*
+    This class contains a 'recipe' for each turn cycle,
+    These are validate_board_placement, validate_tiles, validate_word,
+    update_removed_tiles, calculate_score, update_score
+*/
 
 public class TurnConstructor {
 
