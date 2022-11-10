dbf3ef60787c1fc915c69ba3c2c344dba8eb3e1b
pete johansson <petesaiacct@gmail.com>
2022-11-10 14:26:47 -0500

    Dictionary class reads txt file into class and creats an Arraylist of words, and a searchable Hashmap using the getCharacterSetDictionary(String s) and returns an array list of all words that can be combined using the letters of the string. It does this by turning the letters into a SET value. Set Values are the Keys for the dictionary HashMap. So it will return a now usable list for other functions to employ in different ways. For example verify word can check if in list. Find words for AIPlayer can utilize list to iterate or search.

--- src/main/java/CoreEntities/GameParts/Dictionary.java
+++ src/main/java/CoreEntities/GameParts/Dictionary.java
@@ -1,16 +1,64 @@
 package CoreEntities.GameParts;
 /* This class reads the dictionary.txt file and imports it as a searchable file for use to confirm
 words with verify_word for validity to place on board.
-
-
  */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Dictionary {
 
-    /*
-    TODO Establish data type with co-members, create constructor style to read file and create object.
+    private ArrayList<String> longDictionary;
 
-    TODO Create search methods for length and character sets.
+    private HashMap<Set<Character>, ArrayList<String>> characterSetDictionary;
 
-     */
+    public Dictionary() throws FileNotFoundException {
+        myDictionaryReader();
+        charSetDictionary();
+    }
+
+    public void myDictionaryReader() throws FileNotFoundException {
+
+        Scanner s = new Scanner(new File("src/resources/scrabble_dictionary.txt"));
+        ArrayList<String> list = new ArrayList<>();
+        while (s.hasNextLine()){
+            list.add(s.nextLine());
+        }
+        s.close();
+        this.longDictionary = list;
+
+
+    }
+    public void charSetDictionary(){
+        HashMap<Set<Character>, ArrayList<String>> setDict = new HashMap<>();
+        ArrayList<String> list;
+        for (String s: this.longDictionary) {
+            if(setDict.containsKey(strToSet(s))){
+                list = setDict.get(strToSet(s));
+                list.add(s);}
+            else {
+                list = new ArrayList<>();
+                list.add(s);
+                setDict.put(strToSet(s), list);}
+            }
+        this.characterSetDictionary = setDict;
+    }
+    private static Set<Character> strToSet(final String str) {
+        Set<Character> set;
+
+        if (str == null) {
+            return new HashSet<>();
+        }
+        set = new HashSet<>(str.length());
+        for (int i = 0; i < str.length(); i++) {
+            set.add(str.charAt(i));
+        }
+        return set;
+    }
+
+    public ArrayList<String> getCharacterSetDictionary(String s){
+
+        return characterSetDictionary.get(strToSet(s));
+    }
 }
