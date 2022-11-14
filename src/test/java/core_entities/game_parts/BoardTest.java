8436874744f96bfad3f0a1ed04992fe1aa9da0d8
ethanhonest <ethan.honest@mail.utoronto.ca>
2022-11-14 18:23:07 -0500

    Changed BoardTest to ignore player

--- src/test/java/core_entities/game_parts/BoardTest.java
+++ src/test/java/core_entities/game_parts/BoardTest.java
@@ -1,29 +1,15 @@
 package core_entities.game_parts;
 
-import CoreEntities.GameParts.Bag;
 import CoreEntities.GameParts.Tile;
-import CoreEntities.Player.HumanPlayer;
-import CoreEntities.Player.Player;
-import CoreEntities.Player.ComputerPlayer;
 
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
-    Bag bag;
-    LetterRack rackOne;
-    LetterRack rackTwo;
-    Player p1;
-    Player p2;
     Board board;
     BoardFactory factory;
-    @Before
+    @BeforeEach
     public void setup() {
-        bag = new Bag();
-        rackOne = new LetterRack(bag, 7);
-        rackTwo = new LetterRack(bag, 7);
-        p1 = new HumanPlayer("Ethan", rackOne, 0);
-        p2 = new ComputerPlayer("BeepBoop", rackTwo, 0);
         factory = new DefaultBoardFactory();
         board = factory.create();
     }
@@ -36,6 +22,8 @@ class BoardTest {
             tileList[i] = new Tile('a', i);
         }
         board.placeTiles(tileList, c1, c2);
-        assert('a' == board.getGrid()[1][2].getTile().getLetter());
+        for (int i = 2; i <= 6; i++) {
+            assert ('a' == board.getGrid()[1][i].getTile().getLetter());
+        }
     }
 }
