13dbba0811962f1791e0f1bd652f3f6bc948fe3d
Mina Chiang <mina.chiang@mail.utoronto.ca>
2022-11-16 15:15:17 -0500

    Working on method generateMove()

--- src/main/java/CoreEntities/Player/ComputerPlayer.java
+++ src/main/java/CoreEntities/Player/ComputerPlayer.java
@@ -1,10 +1,39 @@
 package CoreEntities.Player;
 
 import core_entities.game_parts.LetterRack;
+import CoreEntities.GameParts.Tile;
 
 public class ComputerPlayer extends Player{
     public ComputerPlayer(String name, LetterRack rack, int score) {
         super(name, rack, score);
+    }
+    /**
+     * generate a valid word that can be placed on the board
+     * use the makeMove method to place the word
+     */
+    void generateMove(){
+
+        String result = new String();
+        char[] my_char = new char[];
+
+        int i = 0;
+        for (Tile letters: rack){
+            my_char[i] = letters.getLetter();
+            i ++;
+        }
+
+
+        if (result.equals("")){
+
+        }
+        else {
+            if (makeMove(result)){
+                rack.remove(result);
+                rack.refill();
+            }
+            else {}
+
+        }
     }
 
     @Override
