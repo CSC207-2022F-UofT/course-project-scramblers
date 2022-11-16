225e7bcd72e3707126f9e3040ee1898a339d0677
Joseph Sun <jsphsun@gmail.com>
2022-11-16 15:42:59 -0500

    Added the files needed for user story and minor edit to LetterRack.java documentation.

--- src/main/java/exchange_rack_letters/ExchangeLettersInteractor.java
+++ src/main/java/exchange_rack_letters/ExchangeLettersInteractor.java
@@ -1,22 +1,10 @@
 package exchange_rack_letters;
-import core_entities.game_parts.*;
+import core_entities.game_parts.Tile;
 
 public class ExchangeLettersInteractor implements ExchangeLettersInputBoundary{
 
-    private GameState gameRef;
-
-    public ExchangeLettersInteractor(GameState gameInputRef){
-        this.gameRef = gameInputRef;
-    }
-
-    /**
-     * executes the operation to exchange characters from the bag and player's letter rack
-     * the exhcanged letters are the input tiles
-     * @param tileInputArray
-     * @return true if method succeeds and fails if not.
-     */
     @Override
-    public boolean exchangeLetters(Tile[] tileInputArray){
+    public boolean execute(Tile[] tileInputArray){
 
         return true;
     }
