08b663c148c8f1e463c65e957e7d2c7436abadfd
Joseph Sun <jsphsun@gmail.com>
2022-11-16 15:42:59 -0500

    Added the files needed for user story and minor edit to LetterRack.java documentation.

---
+++ src/main/java/exchangeRackLetters/ExchangeLettersInputBoundary.java
@@ -0,0 +1,10 @@
+package exchangeRackLetters;
+import core_entities.game_parts.Tile;
+
+public interface ExchangeLettersInputBoundary {
+
+    /**
+     *
+     */
+    boolean execute(Tile[] tileInputArray);
+}
