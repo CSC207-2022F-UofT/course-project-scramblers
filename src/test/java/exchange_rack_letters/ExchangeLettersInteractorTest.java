f15b73ae1639f0bba898089ab5db11c9debef617
Joseph Sun <jsphsun@gmail.com>
2022-11-18 12:25:38 -0500

    Added Signatures for all the tests.

--- src/test/java/exchange_rack_letters/ExchangeLettersInteractorTest.java
+++ src/test/java/exchange_rack_letters/ExchangeLettersInteractorTest.java
@@ -1,7 +1,50 @@
 package exchange_rack_letters;
+import CoreEntities.Player.HumanPlayer;
 import core_entities.game_parts.*;
+import org.junit.jupiter.api.*;
 
 public class ExchangeLettersInteractorTest {
 
+    GameState gameState;
+
+    @BeforeEach
+    void setup(){
+        Bag bagRef = new Bag();
+        gameState = new GameState(new HumanPlayer("bob", new LetterRack(bagRef, 7), 0),
+                new HumanPlayer("bob2", new LetterRack(bagRef, 7), 0),
+                new DefaultBoard());
+    }
+
+    /**
+     * basic working case
+     */
+    @Test
+    void BaseWorkingCase(){
+
+    }
+
+    /**
+     * Exchange all letters
+     */
+    @Test
+    void AllLettersSwap(){
+
+    }
+
+    /**
+     * No letters wanted to be swapped so the array is empty.
+     */
+    @Test
+    void NoLetterSwap(){
+
+    }
+
+    /**
+     * Some/all letters no present in rack
+     */
+    @Test
+    void LettersNotPresent(){
+
+    }
 
 }
