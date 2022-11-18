f741a1260d7dd5ab5ad071c964b291da6bced6f3
Joseph Sun <jsphsun@gmail.com>
2022-11-18 12:10:06 -0500

    Defined the inputBoundary and interactor for the user story of ExchangeLettersInteractor.java. Rest is yet to be completed.

--- src/main/java/exchange_rack_letters/ExchangeLettersInteractor.java
+++ src/main/java/exchange_rack_letters/ExchangeLettersInteractor.java
@@ -1,10 +1,22 @@
 package exchange_rack_letters;
-import core_entities.game_parts.Tile;
+import core_entities.game_parts.*;
 
 public class ExchangeLettersInteractor implements ExchangeLettersInputBoundary{
 
+    private GameState gameRef;
+
+    public ExchangeLettersInteractor(GameState gameInputRef){
+        this.gameRef = gameInputRef;
+    }
+
+    /**
+     * executes the operation to exchange characters from the bag and player's letter rack
+     * the exhcanged letters are the input tiles
+     * @param tileInputArray
+     * @return true if method succeeds and fails if not.
+     */
     @Override
-    public boolean execute(Tile[] tileInputArray){
+    public boolean exchangeLetters(Tile[] tileInputArray){
 
         return true;
     }
