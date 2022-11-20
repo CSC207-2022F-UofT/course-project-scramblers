ac1d5dd91ee4e32f9c9468468e1f2bf36eaac814
ethanhonest <ethan.honest@mail.utoronto.ca>
2022-11-19 23:25:58 -0500

    Created initial functionality for my use case. It should work as intended, but I would like to add factories for all the entities (workload and time permitting), as the current solution looks fairly ugly

--- src/main/java/launch_new_game_use_case/LaunchNewGameInteractor.java
+++ src/main/java/launch_new_game_use_case/LaunchNewGameInteractor.java
@@ -1,19 +1,37 @@
 package launch_new_game_use_case;
 
-import core_entities.game_parts.BoardFactory;
-import core_entities.game_parts.GameState;
+import CoreEntities.Player.*;
+import core_entities.game_parts.*;
 
 public class LaunchNewGameInteractor implements LaunchGameInputBoundary{
-    private final BoardFactory factory;
+    private final LaunchGameRequestModel inputData;
     private final LaunchGameOutputBoundary presenter;
-    public LaunchNewGameInteractor (BoardFactory factory, LaunchGameOutputBoundary presenter) {
+    private final BoardFactory factory;
+    public LaunchNewGameInteractor (LaunchGameRequestModel inputData, LaunchGameOutputBoundary presenter, BoardFactory factory) {
+        this.inputData = inputData;
         this.factory = factory;
         this.presenter = presenter;
     }
     @Override
     public void createGameState() {
-        GameState.setBoard(factory.create());
-        //add players to game state
+        Bag bag = new Bag();
+        LetterRack p1rack = new LetterRack(bag, 7);
+        LetterRack p2rack = new LetterRack(bag, 7);
+        Player p1;
+        Player p2;
+        if (inputData.getPlayerOneType().equals("Human Player")) {
+            p1 = new HumanPlayer(inputData.getPlayerOneName(), p1rack, 0);
+        }
+        else {
+            p1 = new ComputerPlayer(inputData.getPlayerOneName(), p1rack, 0);
+        }
+        if (inputData.getPlayerTwoType().equals("Human Player")) {
+            p2 = new HumanPlayer(inputData.getPlayerTwoName(), p2rack, 0);
+        }
+        else {
+            p2 = new ComputerPlayer(inputData.getPlayerTwoName(), p2rack, 0);
+        }
+        GameState gameState = new GameState(p1, p2, factory.create());
         presenter.updateViewModel(GameState.getBoard().getMultiplierGrid());
     }
 }
