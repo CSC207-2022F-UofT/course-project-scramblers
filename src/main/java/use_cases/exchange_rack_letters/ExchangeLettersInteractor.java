38dc5767c9bff9d880e22703c1106ab1c635b8cc
Joseph Sun <jsphsun@gmail.com>
2022-11-21 16:09:27 -0500

    Finished the initial version of ExchangeLettersInteractor.java

--- src/main/java/use_cases/exchange_rack_letters/ExchangeLettersInteractor.java
+++ src/main/java/use_cases/exchange_rack_letters/ExchangeLettersInteractor.java
@@ -1,8 +1,15 @@
 package use_cases.exchange_rack_letters;
 
+import CoreEntities.Player.HumanPlayer;
+import CoreEntities.Player.Player;
+import core_entities.game_parts.*;
+
 public class ExchangeLettersInteractor implements ExchangeLettersInputBoundary{
 
-    public ExchangeLettersInteractor(){}
+    private final Bag BAG_REFERENCE;
+    public ExchangeLettersInteractor(Bag inputBag){
+        BAG_REFERENCE = inputBag;
+    }
 
     /**
      * executes the operation to exchange characters from the bag and player's letter rack
@@ -12,8 +19,29 @@ public class ExchangeLettersInteractor implements ExchangeLettersInputBoundary{
     @Override
     public boolean exchangeLetters(String inputString){
 
+        if(inputString == null || inputString.length() == 0){ return false; }
 
+        //placeholder (Will be modified later)
+        Player playerRef = new HumanPlayer("yoot", new LetterRack(new Bag(), 7), 0);
+
+        Tile[] tileArray = convertStringToTileArray(inputString);
+        playerRef.getRack().removeLetters(inputString);
+        for (Tile tile: tileArray){
+            BAG_REFERENCE.add(tile);
+        }
 
         return true;
+    }
+
+    private Tile[] convertStringToTileArray(String inputString){
+
+        Tile[] outputArray = new Tile[inputString.length()];
+        int index = 0;
+
+        for(char character: inputString.toCharArray()){
+            outputArray[index] = new Tile(character);
+        }
+
+        return outputArray;
     }
 }
