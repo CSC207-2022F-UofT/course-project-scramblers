05c7217359a329afb40e3b6be9c8c11fe9ce61b8
JosephUofT <byu.hamdawy>
2022-11-21 01:44:45 -0500

    Adding to the HomeWindow a title

--- src/main/java/CoreEntities/IO/HomeWindow.java
+++ src/main/java/CoreEntities/IO/HomeWindow.java
@@ -8,6 +8,11 @@ public class HomeWindow {
     private JFrame frame;
     private int height;
     private int width;
+    JLabel title = new JLabel("WELCOME TO SCRABBLE!",((Integer) (width/2)));
+
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    Image img = toolkit.getImage("207Logo");
+
 
     public HomeWindow(int w, int h){
 
@@ -15,13 +20,20 @@ public class HomeWindow {
         width = w;
         height = h;
 
+
     }
 
 
     public void setUpHomeWindow(){
         frame.setSize(width, height);
         frame.setTitle("SCRABBLE");
-        frame.setVisible(true);
+
         frame.getContentPane().setBackground(Color.getHSBColor((float) 0.6,(float) 0.40,(float) 1.0));
+        frame.getContentPane().add(title);
+        title.setVisible(true);
+
+        frame.setVisible(true); // keep this last in the method so that everything shows up properly
+
     }
+
 }
