b4271159bd9527a4ddebeabcf0ebe2575f8b496b
JosephUofT <byu.hamdawy>
2022-11-21 09:25:12 -0500

    Creating getters for the View's frame object, the View frame's height and the View frame's width.

--- src/main/java/CoreEntities/IO/View.java
+++ src/main/java/CoreEntities/IO/View.java
@@ -1,11 +1,11 @@
 package CoreEntities.IO;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
 
 public class View{
 
-    private JFrame frame;
+    private static JFrame frame;
     private int height;
     private int width;
 
@@ -16,14 +16,59 @@ public class View{
         height = h;
     }
 
-    public void setUpView(){
-        frame.setSize(width, height);
-        frame.setTitle("SCRABBLE");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.getContentPane().setBackground(Color.getHSBColor((float) 0.6,(float) 0.40,(float) 1.0));
-        //frame.getContentPane().setBackground(Color.getHSBColor((float) 0.36, (float) 0.2, (float) (1.0)));
-
+    /*
+    Getter Method for the View Frame
+    */
+    public JFrame getFrame(){
+        return frame;
     }
 
+    /*
+    Getter Method for the View Frame's width
+    */
+    public int getWidth(){
+        return width;
+    }
+
+    /*
+    Getter Method for the View Frame's height
+    */
+    public int getHeight(){
+        return height;
+    }
+
+
+
+
+
+//    public void setUpView(){
+//        frame.setSize(width, height);
+//        frame.setTitle("SCRABBLE");
+//        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//        frame.setVisible(true);
+//        frame.getContentPane().setBackground(Color.getHSBColor((float) 0.6,(float) 0.40,(float) 1.0));
+//    }
+
+//    public void errorWindow(String msg){
+//        JOptionPane errorframe = new JOptionPane();
+//        JButton button = new JButton("OK");
+//        button.setSize(10,20);
+//        final JTextField errorText = new JTextField(msg);
+//        errorframe.setSize(300,200);
+//        errorframe.setVisible(true);
+//
+//
+//    }
+
+
+
+
+    public static void main(String[] args) {
+        //View v = new View(600,600);
+
+        HomeWindow home = new HomeWindow(frame);
+        home.setUpHomeWindow();
+
+
+    }
 }
