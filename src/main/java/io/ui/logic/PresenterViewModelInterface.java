19a42448afde4e66031b394ad3fde53979c95699
Joseph Sun <jsphsun@gmail.com>
2022-11-28 13:37:31 -0500

    Removed Redundant keywords.

--- src/main/java/io/ui/logic/PresenterViewModelInterface.java
+++ src/main/java/io/ui/logic/PresenterViewModelInterface.java
@@ -1,6 +1,6 @@
 package io.ui.logic;
 
 public interface PresenterViewModelInterface {
-    public void setMessageText(String messageText);
-    public void setBoard(String[][] newBoard);
+    void setMessageText(String messageText);
+    void setBoard(String[][] newBoard);
 }
