b1666e432c98452302097900f6f79b1c73d722d7
ethanhonest <ethan.honest@mail.utoronto.ca>
2022-11-28 12:00:18 -0500

    Started the creation of main

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import CoreEntities.IO.*;
+
+public class Main {
+    public static void main (String [] args) {
+        //View view = new View();
+        //ViewModel viewModel = new ViewModel();
+        Presenter p = new Presenter();
+    }
+}
