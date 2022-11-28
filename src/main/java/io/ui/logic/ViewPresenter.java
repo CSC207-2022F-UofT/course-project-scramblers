dfc4da63e73e36c3c60797e37e30dbdbf0541522
Joseph Sun <jsphsun@gmail.com>
2022-11-28 07:57:00 -0500

    Initial version of ViewPresenter.java

--- src/main/java/io/ui/logic/ViewPresenter.java
+++ src/main/java/io/ui/logic/ViewPresenter.java
@@ -1,4 +1,27 @@
 package io.ui.logic;
 
-public class ViewPresenter {
+import launch_new_game_use_case.LaunchGameOutputBoundary;
+import place_word_refill_user_story.PlaceWordRefillPresenter;
+
+public class ViewPresenter implements LaunchGameOutputBoundary {
+
+    private final PresenterViewModelInterface VIEW_MODEL_INTERFACE;
+
+    /**
+     * Default constructor
+     */
+    ViewPresenter(PresenterViewModelInterface newInterface){
+        VIEW_MODEL_INTERFACE = newInterface;
+    }
+
+    @Override
+    public void updateViewModel(String[][] boardData) {
+        this.VIEW_MODEL_INTERFACE.setBoard(boardData);
+    }
+
+    @Override
+    public void prepareFailView(String csv_file_not_found) {
+        this.VIEW_MODEL_INTERFACE.setMessageText(csv_file_not_found);
+    }
+
 }
