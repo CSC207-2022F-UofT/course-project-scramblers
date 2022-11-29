d1c2e701d0575bd94d4bbfe35f416825ea666e8b
Joseph Sun <jsphsun@gmail.com>
2022-11-29 12:44:52 -0500

    Modified code ling to avoid breaking the code line rule.

--- src/main/java/launch_new_game_use_case/LaunchNewGameInteractor.java
+++ src/main/java/launch_new_game_use_case/LaunchNewGameInteractor.java
@@ -6,18 +6,24 @@ import core_entities.game_parts.*;
 import java.io.IOException;
 
 public class LaunchNewGameInteractor implements LaunchGameInputBoundary{
+    private final LaunchGameRequestModel inputData;
     private final LaunchGameOutputBoundary presenter;
     private final LaunchGameDataAccessObject dataAccessObject;
     private final CreateDictionaryDataAccessObject dictionaryAccessObject;
     private final BoardFactory factory;
-    public LaunchNewGameInteractor (LaunchGameOutputBoundary presenter, LaunchGameDataAccessObject dataAccessObject, CreateDictionaryDataAccessObject dictionaryAccessObject, BoardFactory factory) {
+    public LaunchNewGameInteractor (LaunchGameRequestModel inputData,
+                                    LaunchGameOutputBoundary presenter,
+                                    LaunchGameDataAccessObject dataAccessObject,
+                                    CreateDictionaryDataAccessObject dictionaryAccessObject,
+                                    BoardFactory factory) {
+        this.inputData = inputData;
         this.factory = factory;
         this.dataAccessObject = dataAccessObject;
         this.dictionaryAccessObject = dictionaryAccessObject;
         this.presenter = presenter;
     }
     @Override
-    public void createGameState(LaunchGameRequestModel inputData) {
+    public void createGameState() {
         Bag bag = new Bag();
         LetterRack p1rack = new LetterRack(bag, 7);
         LetterRack p2rack = new LetterRack(bag, 7);
@@ -39,7 +45,8 @@ public class LaunchNewGameInteractor implements LaunchGameInputBoundary{
         GameState.setP2(p2);
         try {
             //GameState.setDictionary(dictionaryAccessObject.getDictionaryFile());
-            GameState.setBoard(factory.create(dataAccessObject.createBoardMultiplierGrid(inputData.getBoardCsvFile())));
+            GameState.setBoard(factory.create(dataAccessObject.createBoardMultiplierGrid()));
+            //presenter.updateViewModel(GameState.getBoard().getMultiplierGrid());
             presenter.updateViewModel(new LaunchGameResponseModel(GameState.getBoard().getMultiplierGrid()));
         }
         catch (IOException e) {
