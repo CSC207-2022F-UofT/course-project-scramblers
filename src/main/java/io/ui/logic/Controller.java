cf492822e2b52259fbe06777b2225c651437fb91
Joseph Sun <jsphsun@gmail.com>
2022-11-30 15:32:43 -0500

    added public key word to controller constructor.

--- src/main/java/io/ui/logic/Controller.java
+++ src/main/java/io/ui/logic/Controller.java
@@ -1,9 +1,8 @@
 package io.ui.logic;
 
-import core_entities.game_parts.Coordinate;
-import core_entities.game_parts.GameState;
-import launch_new_game_use_case.*;
-import Take_Turn.*;
+import launch_new_game_use_case.LaunchGameInputBoundary;
+import launch_new_game_use_case.LaunchGameRequestModel;
+import place_word_refill_user_story.PlaceWordInputBoundary;
 import use_cases.reload_game_use_case.*;
 
 public class Controller {
@@ -15,12 +14,13 @@ public class Controller {
 
     //Constructor that's going to be used.
     public Controller(LaunchGameInputBoundary launchGameInput,
-                      ReloadGameInputBoundary reloadGameInputBoundary,
-                      TakeTrunInputBoundary takeTrunInputBoundary){
-        this.LAUNCH_GAME_REF = launchGameInput;
-        this.RELOAD_GAME_REF = reloadGameInputBoundary;
-        this.TAKE_TURN_INPUT_REF = takeTrunInputBoundary;
+               ReloadGameInputBoundary reloadGameInputBoundary,
+               PlaceWordInputBoundary placeWordInputBoundary){
+        LAUNCH_GAME_REF = launchGameInput;
+        RELOAD_GAME_REF = reloadGameInputBoundary;
+        PLACE_WORD_INPUT_REF = placeWordInputBoundary;
     }
+
     public void launchTheGame(){
         LAUNCH_GAME_REF.createGameState(new LaunchGameRequestModel("Human Player",
                 "Human Player",
@@ -32,27 +32,11 @@ public class Controller {
         RELOAD_GAME_REF.reloadGame();
     }
 
-    /**
-     * Take an action given in an input (-1,-1) for exchanging letters and place a word otherwise.
-     * @param inputWord the input word
-     * @param startX the start coordinate
-     * @param startY the end coordinate
-     */
-    public void executeTurn(String inputWord, String startX, String startY) {
-        if(!(startX.contains("[a-zA-Z]") || startY.contains("[a-zA-Z]"))){
-            int startXInt = Integer.parseInt(startX);
-            int startYInt = Integer.parseInt(startY);
-            if(startXInt + inputWord.length() < GameState.getBoard().getLetterGrid().length){
-                TAKE_TURN_INPUT_REF.taketurn(new TakeTrunInputData(inputWord,
-                        new Coordinate(startXInt, startYInt),
-                        new Coordinate(startXInt + inputWord.length(), startYInt)));
-            }
-            if(startYInt + inputWord.length() < GameState.getBoard().getLetterGrid().length){
-                TAKE_TURN_INPUT_REF.taketurn(new TakeTrunInputData(inputWord,
-                        new Coordinate(startXInt, startYInt),
-                        new Coordinate(startXInt, startYInt + inputWord.length())));
-            }
-
+    public void placeWordExecute(String inputWord){
+        if(isOnlyAlphabetString(inputWord)){
+            int x = 0; //place hold
+            //PLACE_WORD_INPUT_REF.placeWordRefill();
         }
+
     }
 }
