c3345cb05345c82fc6dd338686f35611ea3c9d41
Mina Chiang <mina.chiang@mail.utoronto.ca>
2022-11-30 15:57:16 -0500

    Create use case for the take turn.
    and add two more method for presenter

--- src/main/java/Take_Turn/TakeTurnInteractor.java
+++ src/main/java/Take_Turn/TakeTurnInteractor.java
@@ -4,19 +4,27 @@ import core_entities.game_parts.GameState;
 import place_word_refill_user_story.PlaceWordInputBoundary;
 import place_word_refill_user_story.PlaceWordRefillInteractor;
 import place_word_refill_user_story.PlaceWordRefillResponseModel;
+import use_cases.exchange_rack_letters.ExchangeLettersInputBoundary;
+import use_cases.exchange_rack_letters.ExchangeLettersInteractor;
+import use_cases.save_game_use_case.SaveGameInputBoundary;
+import use_cases.save_game_use_case.SaveGameInteractor;
 
 public class TakeTurnInteractor implements TakeTrunInputBoundary{
 
     private final TakeTrunInputData inputData;
     private final TakeTurnOutputBoundary presenter;
     private final PlaceWordInputBoundary placeword;
+    private final SaveGameInputBoundary saveGame;
     private static int tries = 0;
 
 
-    public TakeTurnInteractor(TakeTrunInputData inputData, TakeTurnOutputBoundary presenter, PlaceWordInputBoundary placeword) {
+    public TakeTurnInteractor(TakeTrunInputData inputData, TakeTurnOutputBoundary presenter,
+                              PlaceWordInputBoundary placeword,
+                              SaveGameInputBoundary saveGameInputBoundary) {
         this.inputData = inputData;
         this.presenter = presenter;
         this.placeword = placeword;
+        this.saveGame = saveGameInputBoundary;
     }
 
     @Override
@@ -38,17 +46,25 @@ public class TakeTurnInteractor implements TakeTrunInputBoundary{
             this.presenter.updateBoard(updateinfo);
             tries = 0;
             GameState.changeTurn();
+            saveGame.save();
         }
         else{
             // the word cannot be placed
             tries += 1;
             if (tries == 3){
-                //Joseph's user story exchange letter
+                // exchange tiles with bag
+                ExchangeLettersInputBoundary b = new ExchangeLettersInteractor();
+                b.exchangeLetters(this.inputData.getWord());
+                TakeTurnOutputData updateinfo1 = new TakeTurnOutputData(this.inputData.getWord(),
+                        this.inputData.getCurrplayer().getRack(), this.inputData.getStart(), this.inputData.getEnd());
+                this.presenter.updateRack(updateinfo1);
                 GameState.changeTurn();
-            }
+                saveGame.save();
+                }
             else{
                 // show the view model that ask the player to try again
-                presenter.prepareFailView("Word cannot be placed, please try again");
+                String error = "Word cannot be placed, please try again" + (3 - tries) + "remaining";
+                presenter.prepareFailView(error);
             }
             }
     }
--- src/main/java/io/ui/logic/Presenter.java
+++ src/main/java/io/ui/logic/Presenter.java
@@ -1,10 +1,12 @@
 package io.ui.logic;
+import Take_Turn.TakeTurnOutputBoundary;
+import Take_Turn.TakeTurnOutputData;
 import launch_new_game_use_case.LaunchGameOutputBoundary;
 import launch_new_game_use_case.LaunchGameResponseModel;
 import place_word_refill_user_story.PlaceWordRefillOutputBoundary;
 import place_word_refill_user_story.PlaceWordRefillResponseModel;
 
-public class Presenter implements LaunchGameOutputBoundary, PlaceWordRefillOutputBoundary {
+public class Presenter implements LaunchGameOutputBoundary, TakeTurnOutputBoundary, PlaceWordRefillOutputBoundary{
 
     private final PresenterViewModelInterface VIEW_MODEL_INTERFACE;
 
@@ -23,6 +25,16 @@ public class Presenter implements LaunchGameOutputBoundary, PlaceWordRefillOutpu
     @Override
     public void updateViewModel(LaunchGameResponseModel responseModel) {
         this.VIEW_MODEL_INTERFACE.setBoard(responseModel.getBoardLayout());
+    }
+
+    @Override
+    public void updateBoard(TakeTurnOutputData updateinfo) {
+        // update the board and the rack based on the given info
+    }
+
+    @Override
+    public void updateRack(TakeTurnOutputData updateinfo) {
+        // update the rack based on the given info
     }
 
     @Override
