a35f9be2f59e20e5cf74d342294c5fe5f0165646
Mojan Nooshin Habibi <habibi.mojan@gmail.com>
2022-12-02 10:37:14 -0500

    Merge remote-tracking branch 'origin/7-humanplayerjava-creation' into 7-humanplayerjava-creation
    # Conflicts:
    #	src/main/java/CoreEntities/Player/ComputerPlayer.java
    #	src/main/java/CoreEntities/Player/HumanPlayer.java
    #	src/main/java/CoreEntities/Player/Player.java

--- src/main/java/CoreEntities/Player/HumanPlayer.java
+++ src/main/java/CoreEntities/Player/HumanPlayer.java
@@ -1,7 +1,11 @@
 package CoreEntities.Player;
 
 
+import core_entities.game_parts.Dictionary;
 import core_entities.game_parts.LetterRack;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class HumanPlayer extends Player {
 
@@ -13,18 +17,33 @@ public class HumanPlayer extends Player {
     /**
      *
      * @param word
-     * @return true if word is in dictionary. Otherwise, return
-     * a str, telling Player to try again.
+     * @return true if word is in dictionary. Otherwise, return false.
      */
-    public chooseWord(String word) {
-        // body here.
+    // check to see if word is in dictionary
+    // Dictionary.getCharacterSetDictionary returns an array list of
+    // all words in dictionary containing the letters in a str
+    // if word in result, return true
+    // else, return false
+    public boolean chooseWord(String word) {
+        ArrayList<String> availableWords = Dictionary.getCharacterSetDictionary(word);
+        for (int i = 0; i < availableWords.toArray().length; i++) {
+            if (Objects.equals(word, availableWords.get(i))) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
     }
-
 
     // Option a) Exchange Letters -> Jos
     //           - Player has to use option a if tries = 3
     // Option b) Player makes move
-    //
+    // i. Player chooses a valid word - chooseWord
+    // ii. Player chooses a valid space on board --> Hrid
+    //     - run update_tries. If tries = 3, Player goes to Option a
+    // iii. Update LetterRack -> Jos
+    // iv. Change_turn to next Player -> Ethan
     @Override
     boolean makeMove(String word, int start_x, int end_y) {
 
