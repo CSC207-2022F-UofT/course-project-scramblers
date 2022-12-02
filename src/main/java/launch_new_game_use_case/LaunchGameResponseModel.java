3665cd1616968b9282456317b42185d9bc178834
ethanhonest <ethan.honest@mail.utoronto.ca>
2022-12-02 17:43:38 -0500

    Minor changes to launch game response model to include letter rack

--- src/main/java/launch_new_game_use_case/LaunchGameResponseModel.java
+++ src/main/java/launch_new_game_use_case/LaunchGameResponseModel.java
@@ -2,10 +2,13 @@ package launch_new_game_use_case;
 
 public class LaunchGameResponseModel {
     private final String [][] boardLayout;
-    public LaunchGameResponseModel (String [][] boardLayout) {
+    private final char [] letterRack;
+    public LaunchGameResponseModel (String [][] boardLayout, char [] letterRack) {
         this.boardLayout = boardLayout;
+        this.letterRack = letterRack;
     }
     public String[][] getBoardLayout() {
         return this.boardLayout;
     }
+    public char[] getLetterRack(){return this.letterRack;}
 }
