fc0d91be7754da25baf92f2344517d1c58121110
ethanhonest <ethan.honest@mail.utoronto.ca>
2022-12-06 11:03:10 -0500

    New changes to view final

--- src/main/java/io/ViewFinal.java
+++ src/main/java/io/ViewFinal.java
@@ -1,26 +1,17 @@
 package io;
 
-import core_entities.game_parts.Board;
 import core_entities.game_parts.LetterRack;
-import core_entities.game_parts.Square;
 import io.ui.logic.Controller;
-import io.ui.logic.Presenter;
-import io.ui.logic.ViewModel;
-import launch_new_game_use_case.LaunchGameResponseModel;
 
 import javax.swing.*;
-import javax.tools.Tool;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.image.ImageObserver;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
-public class ViewFinal extends JFrame{
+import java.util.ArrayList;
+
+public class ViewFinal extends JFrame {
 
     private JFrame frame;
     private JPanel masterPanel;
@@ -54,9 +45,13 @@ public class ViewFinal extends JFrame{
     private JButton rerackButton;
     private Controller controller;
     private JLabel score_1;
+    private JLabel end_coordinates_instructions_x;
+    private JLabel end_coordinates_instructions_y;
+    private JTextField end_coordinates_x;
+    private JTextField end_coordinates_y;
 
 
-    public ViewFinal(Controller ctrlor){
+    public ViewFinal(Controller ctrlor) {
         //creating a controller instance
         controller = ctrlor;
 
@@ -80,24 +75,16 @@ public class ViewFinal extends JFrame{
 
 
         //creating the grids
-        gridLayoutBoard = new GridLayout(15,15);
-        gridLayoutMenu = new GridLayout(5,1);
-        gridLayoutRack = new GridLayout(10,1);
-        gridLayoutMaster2 = new GridLayout(5,2);
-
+        gridLayoutBoard = new GridLayout(15, 15);
+        gridLayoutMenu = new GridLayout(15, 1);
+        //gridLayoutRack = new GridLayout(10,1);
+        gridLayoutMaster2 = new GridLayout(5, 2);
 
 
         BoardPanel.setLayout(gridLayoutBoard);
         RackPanel.setLayout(gridLayoutRack);
         MenuPanel.setLayout(gridLayoutMenu);
         //masterPanel2.setLayout(gridLayoutMaster2);
-
-
-        //creating the rerack button and adding it to the rack panel
-        rerackButton = new JButton("Rerack");
-        RackPanel.add(rerackButton);
-        score_1 = new JLabel("Current Player's Score:");
-        RackPanel.add(score_1);
 
 
         //creating menu buttons
@@ -117,9 +104,15 @@ public class ViewFinal extends JFrame{
         MenuPanel.add(csvInstructionLabel);
         MenuPanel.add(csvFilePathField);
 
+
+        //creating the rerack button and adding it to the rack panel
+        rerackButton = new JButton("Rerack");
+        MenuPanel.add(rerackButton);
+        score_1 = new JLabel("Current Player's Score:");
+        MenuPanel.add(score_1);
+
         //creating the board buttons
         List_of_Board_Buttons = ScrabbleBoard();
-
 
 
         //creating the letter rack buttons
@@ -147,6 +140,21 @@ public class ViewFinal extends JFrame{
         coordinates_y = new JTextField();
         masterPanel2.add(coordinates_y);
 
+        //creating the end_coordinates_x instructions label
+        end_coordinates_instructions_x = new JLabel("Please type in the end x coordinate in the textbox below:");
+        masterPanel2.add(end_coordinates_instructions_x);
+
+        //creating the end_coordinates_x textfield
+        end_coordinates_x = new JTextField();
+        masterPanel2.add(end_coordinates_x);
+
+        //creating the end_coordinates_y instructions label
+        end_coordinates_instructions_y = new JLabel("Please type in the end y coordinate in the textbox below");
+        masterPanel2.add(end_coordinates_instructions_y);
+
+        //creating the end_coordinates_y textfield
+        end_coordinates_y = new JTextField();
+        masterPanel2.add(end_coordinates_y);
 
 
         //creating the word instructions JLabel
@@ -226,7 +234,6 @@ public class ViewFinal extends JFrame{
         frame.setVisible(true);
 
 
-
     }
 
 
@@ -234,8 +241,8 @@ public class ViewFinal extends JFrame{
         setScore_1(score);
     }
 
-    public void updateRack(LetterRack rack){
-        for (int i = 0; i<7; i++){
+    public void updateRack(LetterRack rack) {
+        for (int i = 0; i < 7; i++) {
             JButton temp = (JButton) List_of_Letter_Rack_Buttons.get(i);
             char temp_letter = rack.getLETTERS()[i].getLetter();
             temp.setText(String.valueOf(temp_letter));
@@ -243,23 +250,30 @@ public class ViewFinal extends JFrame{
 
     }
 
-    public void updateErrorMsg(String err){
+    public void updateErrorMsg(String err) {
         setError_message_field(err);
     }
 
 
-    public void updateBoard(char [][] b){
+    public void updateBoard(char[][] b) {
         ArrayList<JButton> lst = List_of_Board_Buttons;
         int pos = 0;
-        for (int i = 0; i< 15; i++){
-            for (int j = 0; j<15; j++){
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
                 lst.get(pos).setText(String.valueOf(b[j][i]));
                 pos += 1;
             }
         }
     }
 
-    public ArrayList ScrabbleBoard(){
+
+    public static void main(String[] args) {
+
+        ViewFinal view = new ViewFinal(null);
+
+    }
+
+    public ArrayList ScrabbleBoard() {
 
         ArrayList button_list = new ArrayList();
 
@@ -267,12 +281,12 @@ public class ViewFinal extends JFrame{
         int start_point_y = 100;
         int position_i = start_point_x;
 
-        for(int i = 0; i < 15; i++){
+        for (int i = 0; i < 15; i++) {
 
             int position_j = start_point_y;
-            for( int j = 0; j < 15; j++){
+            for (int j = 0; j < 15; j++) {
                 JButton button = new JButton(" ");
-                button.setSize(5,5);
+                button.setSize(5, 5);
                 button.setAlignmentX(position_i);
                 button.setAlignmentY(position_j);
                 BoardPanel.add(button);
@@ -287,28 +301,22 @@ public class ViewFinal extends JFrame{
     }
 
 
-
-
-    public ArrayList RackButtons(){
+    public ArrayList RackButtons() {
 
         ArrayList button_list = new ArrayList();
 
 
-            for( int j = 0; j < 7; j++){
-                JButton button = new JButton(" ");
-                button.setSize(3,3);
-                RackPanel.add(button);
-                button_list.add(button);
+        for (int j = 0; j < 7; j++) {
+            JButton button = new JButton(" ");
+            button.setSize(3, 3);
+            MenuPanel.add(button);
+            button_list.add(button);
 
-            }
+        }
 
 
         return button_list;
     }
-
-
-
-
 
 
     public String getCoordinates_x() {
@@ -319,7 +327,7 @@ public class ViewFinal extends JFrame{
         return coordinates_y.getText();
     }
 
-    public String getScore_1(){
+    public String getScore_1() {
         return score_1.getText();
     }
 
@@ -327,19 +335,8 @@ public class ViewFinal extends JFrame{
         score_1.setText("Current Player's Score: " + score);
     }
 
-    public void setError_message_field(String err){
+    public void setError_message_field(String err) {
         error_message_field.setText(err);
     }
-
-    public void setController(Controller c) {
-        this.controller = c;
-    }
-
-//    public void setColors(){
-//        ArrayList<JButton> lst = List_of_Board_Buttons;
-//        for (int i=0; i<225; i++){
-//
-//        }
-//    }
-
 }
+
