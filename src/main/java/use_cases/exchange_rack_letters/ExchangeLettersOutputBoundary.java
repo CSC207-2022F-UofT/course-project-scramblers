3caa83c7a89841858fc8cc549b568a89ee27b0b5
Joseph Sun <jsphsun@gmail.com>
2022-12-06 10:53:34 -0500

    Removed output interface (already used somewhere else).

--- src/main/java/use_cases/exchange_rack_letters/ExchangeLettersOutputBoundary.java
+++
@@ -1,8 +0,0 @@
-package use_cases.exchange_rack_letters;
-
-public interface ExchangeLettersOutputBoundary {
-    /**
-     * call for the viewModel class to be updated to reflect changes.
-     */
-    void updateViewModelAfterExchange();
-}
--- src/test/java/use_cases/exchange_rack_letters/ExchangeLettersInteractorTest.java
+++ src/test/java/use_cases/exchange_rack_letters/ExchangeLettersInteractorTest.java
@@ -17,13 +17,7 @@ public class ExchangeLettersInteractorTest {
         final Bag BAG_REF = new Bag();
         GameState.setP1(new HumanPlayer("bob", new LetterRack(BAG_REF, 7), 0));
         rand = new Random('1');
-        ExchangeLettersOutputBoundary inputInterface = new Presenter() {
-            @Override
-            public void updateViewModelAfterExchange() {
-                result = true;
-            }
-        };
-        inputBoundary = new ExchangeLettersInteractor(inputInterface);
+        inputBoundary = new ExchangeLettersInteractor();
     }
 
     /**
